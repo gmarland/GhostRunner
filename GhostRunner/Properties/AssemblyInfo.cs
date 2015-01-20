3a179cdecd266b39a2c9517ce6ef815be318d5af
Gareth Marland <gareth.marland@gmail.com>
2015-01-19 21:30:08 -0500

    Updating assembly version

--- GhostRunner/Properties/AssemblyInfo.cs
+++ GhostRunner/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 // added logging for Log4Net
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "Log4Net.config", Watch = true)]
